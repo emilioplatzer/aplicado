b7a4eaa35197a3686df627efc22710de8d26eca5
Emilio Platzer <emilioplatzer@gmail.com>
2020-05-18 23:03:01 -0300

    Primer commit

---
+++ src/server/noticias.ts
@@ -0,0 +1,80 @@
+import * as express from "express";
+
+import * as open from "open";
+import { Server } from "http";
+
+function conclude(resolve:()=>void, reject:(err?:Error|undefined)=>void, message?:string){
+    return function(err?:Error|undefined){
+        if(err){
+            reject(err);
+        }else{
+            if(message){
+                console.log(message);
+            }
+            resolve()
+        }
+    }
+}
+
+class EasyServer{
+    private app=express();
+    private server?:Server;
+    private killed?:true;
+    constructor(){
+    }
+    async startListening():Promise<void>{
+        this.app.get('/lista', function(_req, res){
+            res.send('<a href="/kill">Click</a> to stop the server');
+        })
+        this.app.get('/kill', (_req, res)=>{
+            res.send('killing...');
+            console.log('recive kill')
+            this.killed=true;
+        })
+        return new Promise((resolve, reject)=>{
+            console.log('start to listen')
+            this.server = this.app.listen(3303, conclude(resolve, reject, 'listening'));
+        });
+    }
+    async becomesKilled(){
+        return new Promise((resolve)=>{
+            const interval = setInterval(()=>{
+                if(this.killed){
+                    console.log('detect kill')
+                    clearInterval(interval);
+                    resolve()
+                }
+            },1000);
+        })
+    }
+    async stopListening(){
+        console.log('closing...')
+        return new Promise((resolve,reject)=>{
+            if(!this.server){
+                return reject(new Error('server does not started yet'))
+            }
+            this.server.close(conclude(resolve,reject,'closed'))
+        })
+    }
+}
+
+
+async function start(){
+    try{
+        console.log('starting all');
+        console.log('platform',process.platform);
+        const server = new EasyServer();
+        await server.startListening();
+        open('http://localhost:3303/lista');
+        await server.becomesKilled();
+        await server.stopListening();
+        console.log('end of all')
+    }catch(err){
+        console.log('exit wit error')
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+start();
+
