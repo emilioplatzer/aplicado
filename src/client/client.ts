85782397bcfba59017e06f36f65ec5806bc8d331
Emilio Platzer <emilioplatzer@gmail.com>
2020-05-21 14:27:45 -0300

    Intento parciel de separar los tsconfig.json

---
+++ src/client/client.ts
@@ -0,0 +1,7 @@
+window.addEventListener("load", function(){
+    var closeButton = document.getElementById('closeButton');
+    closeButton.addEventListener("click", function () {
+        navigator.sendBeacon('/${entryPoints[entryPoints.kill]}',new Date().toString())
+        close();
+    });
+});
