c72b9c49c77c32037473f3e6ae00734ba9a6c3e2
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2020-05-22 22:05:30 -0300

    faltaba la barra

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,7 @@
 window.addEventListener("load", function(){
     var closeButton = document.getElementById('closeButton');
     closeButton?.addEventListener("click", function () {
-        navigator.sendBeacon('kill',new Date().toString())
+        navigator.sendBeacon('/kill',new Date().toString())
         close();
     });
 });
