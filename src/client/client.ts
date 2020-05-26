abfc992ae0cc560aac530ac5a0fc7ebde2224ad9
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2020-05-26 10:19:40 -0300

    prueba commit con repo cambiado

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,6 +2,7 @@ import { entryPoints } from "./common";
 
 window.addEventListener("load", function(){
     var closeButton = document.getElementById('closeButton');
+    //prueba commit con repo cambiado
     closeButton?.addEventListener("click", function () {
         navigator.sendBeacon(`/${entryPoints[entryPoints.kill]}`, new Date().toString())
         close();
